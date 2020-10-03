8177117d40906a4805e085c9d72aceedfa49ffa2
zin min phyo <zinminphyo@beter.hr>
2020-10-03 18:02:17 +0630

    Initial Commit

---
+++ ViewTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewTesting
+//
+//  Created by zin min phyo on 03/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
